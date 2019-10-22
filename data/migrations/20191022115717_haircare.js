6216bc7e0dd0b377a94425af094a6259a29c6392
Adrian-Garcia <agarc5@hotmail.com>
2019-10-22 15:46:32 -0500

    im sorry/there is so much here :/

---
+++ data/migrations/20191022115717_haircare.js
@@ -0,0 +1,41 @@
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('users', function(users) {
+        users.increments();
+        users
+            .string('username')
+            .notNullable()
+            .unique();
+        users
+            .string('password', 128)
+            .notNullable();
+        users
+            .string('location', 128);
+        users
+            .string('email', 128)
+            .notNullable()
+            .unique();
+      })
+
+      .createTable('stylists', function(stylists) {
+        stylists.increments();
+        stylists
+            .string('username', 128)
+            .notNullable()
+            .unique();
+        stylists
+            .string('about', 256)
+            .notNullable();
+        stylists
+            .string('skills', 128);
+        stylists
+            .string('password', 128)
+            .notNullable();
+        stylists
+            .string('location', 128)
+      });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('stylists').dropTableIfExists('users');
+  };
---
+++ routes/userRouter.js
@@ -0,0 +1,109 @@
+//import express
+const express = require('express');
+
+//import database
+const Users = require('../models/userDb.js');
+// const Posts = require('../data/Helpers/postDb');
+
+const router = express.Router();
+
+const sendError = (msg, res) => {
+    res.status(500).json({ errorMessage: `${msg}` })
+};
+
+const sendMissing = (res) => {
+    res.status(404).json({ errorMessage: 'The User does not exist.' });
+}
+
+//----------MAKE CRUD ENDPOINTS----------
+
+//get requests all users
+    router.get('/', (req, res) => {
+        Users
+        .get()
+        .then( user => {
+        res.status(200).json(user);
+        })
+        .catch( err => {
+        return sendError( 'User information Unavailable at this moment', res );
+        })
+    }
+);
+
+//get user by id 
+    router.get('/:id', /* validateUserId,*/ (req, res) => {
+        res.status(200).json(req.user)  
+    }
+);
+
+//NEW USER using post
+router.post('/',/* validateUser,*/ (req, res) => {
+    Users.insert(req.body)
+        .then(user => {
+            //console.log(user);
+            res.status(200).json({
+                message: 'User created, congratz!!!'
+            });
+        })
+        .catch(error => {
+            //console.log(error);
+            res.status(500).json({
+                message: 'Error creating the user within the post command.'
+            });
+        })
+    }
+);
+
+//update user
+    router.put('/:id',/* validateUserId, validateUser,*/ (req, res) => {
+        //define id 
+        const ID = req.params.id
+    
+        //define req.body
+        const { username, password, location, email } = req.body;
+        const user = { username, password, location, email };
+    
+        //check the req body
+        if(!username || !password ) { 
+        return res.status(400).json({ error: 'Please provide the NEW user name or password' });
+        }
+        Users
+        .update(ID, user)
+        .then( person => {
+        if (person === undefined) {
+            return sendMissing(res);
+        }
+        else{
+            newUser = { ID, username, password, location }
+            return res.status(201).json(newUser);
+        }
+        })
+        .catch( err => {
+        return sendError( 'This function is currently unavailable', res );
+        })
+    }
+);
+
+//delete User
+    router.delete('/:id', (req, res) => {
+        //set id
+        const ID = req.params.id
+        //delete the user
+        Users
+        .remove(ID)
+        .then( user => { 
+        if (user === undefined) {
+            return sendMissing(res);
+        }
+        else{
+            return res.status(200).json(user);
+        }
+        })
+        .catch( err => {
+        return sendError( 'This function is currently unavailable', res );
+        })
+
+    }
+);
+
+module.exports = router;
