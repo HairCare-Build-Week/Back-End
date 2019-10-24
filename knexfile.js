057c37f03099bd52644139708fbeeedeaf64e31e
Adrian-Garcia <agarc5@hotmail.com>
2019-10-24 15:53:42 -0500

    test 4

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // Update with your config settings.
-const dbConnection = process.env.DATABASE_URL;
+// const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
   development: {
@@ -31,9 +31,13 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: dbConnection,
     useNullAsDefault: true,
-    ssl: true,
+    connection: {
+      host: 'salt.db.elephantsql.com',
+      user: 'sztxxlqt',
+      password: 'BaqFcTEqCWQkqewtsvyQz12ejK7lv74g',
+      database: 'sztxxlqt'
+    },
     migrations: {
       directory: './data/migrations',
     },
